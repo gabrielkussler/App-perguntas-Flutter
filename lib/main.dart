b63196b2642fc040dbc1e7f20973016a93998018
gabriel <gkgabrielkussler@gmail.com>
2021-07-01 17:08:51 -0300

    criando List<String> perguntas

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,28 @@ class PerguntaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final perguntas = [
+      'Qual é a sua cor favorita ?',
+      'Qual o seu animal favorito ?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Perguntas'),
         ),
-        body: Text('Ola Flutter'),
+        body: Column(
+          children: [
+            Text(perguntas[1]),
+            RaisedButton(onPressed: null,
+            child: Text('Resposta1 '),
+            ) ,           RaisedButton(onPressed: null,
+            child: Text('Respostas'),
+            ) ,           RaisedButton(onPressed: null,
+            child: Text('Respostas'),
+            )
+          ],
+        ),
       ),
     );
   }
